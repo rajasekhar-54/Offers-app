fbb623f96bcae05d55d5b9516fa6daac8eee888c
rajasekhar <rajasekhar.catchme@gmail.com>
2019-07-25 06:15:04 +0000

    3rd commit code removed in Header

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,5 @@ public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
-      // prints"This is a welocme prints logic"
-      System.out.println("this is new git repository");
    }
 }
